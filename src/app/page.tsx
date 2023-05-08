e1ddfdb06623b517cef44abb95ec3eab3e050297
Dylan L <dylancblarocque@gmail.com>
2023-05-07 20:39:06 -0700

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-      <div className='z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex'>
+      <div className='z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:center'>
         <p className='fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Coming Soon&nbsp;
           <code className='font-mono font-bold'>River City Leaders Forum</code>
@@ -15,32 +16,37 @@ export default function Home() {
         <Image
           className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
           src='/svg/grayscale.svg'
-          alt='Next.js Logo'
+          alt='rclf Logo'
           width={300}
           height={300}
           priority
         />
       </div>
 
-      <div className='grid mb-32 text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <a
+      <div className='grid mb-32 text-center lg:mb-0 lg:grid-cols-2 lg:text-left'>
+        {/* <a
           href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           target='_blank'
           rel='noopener noreferrer'
+        > */}
+        {/* <Link
+          href='/about'
+          className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            About{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find in-depth information about RCLF and our mission.
           </p>
-        </a>
+        </Link> */}
+        {/* </a> */}
 
-        <a
+        {/* <a
           href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
           className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30'
           target='_blank'
@@ -53,11 +59,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Contact us to learn more about RCLF.
           </p>
-        </a>
+        </a> */}
 
-        <a
+        {/* <a
           href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           target='_blank'
@@ -72,7 +78,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore the Next.js 13 playground.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   );
