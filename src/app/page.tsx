a518bed85b26734751a9752ab7271351761093b1
Dylan L <dylancblarocque@gmail.com>
2023-05-08 21:30:59 -0700

    update home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,72 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Navbar from '@/components/NavBar';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-      <div className='z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:center'>
-        <p className='fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Coming Soon&nbsp;
-          <code className='font-mono font-bold'>River City Leaders Forum</code>
-        </p>
-        <div className='fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'></div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/svg/grayscale.svg'
-          alt='rclf Logo'
-          width={300}
-          height={300}
-          priority
-        />
-      </div>
-
-      <div className='grid mb-32 text-center lg:mb-0 lg:grid-cols-2 lg:text-left'>
-        {/* <a
-          href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        > */}
-        {/* <Link
-          href='/about'
-          className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            About{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about RCLF and our mission.
-          </p>
-        </Link> */}
-        {/* </a> */}
-
-        {/* <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Contact us to learn more about RCLF.
-          </p>
-        </a> */}
-
-        {/* <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a> */}
-      </div>
-    </main>
+    <div className='flex justify-center m-2 md:m-12'>
+      <Navbar />
+      <main className='flex flex-col items-center justify-between min-h-screen px-0 md:px-16'>
+        <div className='grid w-full max-w-5xl my-8 md:text-left lg:mb-0 lg:grid-cols-1 lg:text-left'>
+          <div className='px-4 py-0 md:px-5 md:py-4'>
+            <h2 className='mb-3 text-2xl font-semibold'>
+              Empower Your Leadership Journey with RCLF
+            </h2>
+            <p className='text-sm lg:text-base'>
+              A Local Peer Forum for Ambitious Professionals
+            </p>
+            <p className='mt-6 text-sm lg:text-base'>
+              Navigating the world of leadership can be a thrilling yet
+              challenging experience, and connecting with a like-minded
+              community can make all the difference. RCLF is here to provide an
+              accessible and locally focused alternative to traditional
+              high-priced leadership peer forums, designed for CEOs,
+              entrepreneurs, and leaders at all levels.
+            </p>
+            <p className='mt-4 text-sm lg:text-base'>
+              Established in 2020, RCLF has evolved through trial and error to
+              create an optimized format that values your time. Our half-day
+              meetings strike the perfect balance between meaningful interaction
+              and time efficiency, ensuring you get the most out of every
+              session.
+            </p>
+            <p className='mt-4 text-sm lg:text-base'>
+              With a membership fee of just $200 per month, RCLF offers an
+              affordable, high-value experience tailored for ambitious
+              professionals from diverse backgrounds. Here's what you can
+              expect:
+            </p>
+            <ul className='mt-4 text-sm list-disc list-inside lg:text-base'>
+              <li>
+                A carefully curated group of local leaders to engage with and
+                learn from, based on your unique experiences
+              </li>
+              <li>
+                Templates and agenda outlines for effective and organized
+                meetings
+              </li>
+              <li>
+                Facilitation of your first three sessions to kickstart your RCLF
+                journey
+              </li>
+              <li>
+                Quarterly facilitator audits to maintain best practices and
+                improve your experience
+              </li>
+              <li>
+                A commitment to give back 20% of our earnings to the community
+              </li>
+            </ul>
+            <p className='mt-4 text-sm lg:text-base'>
+              RCLF is more than just a peer forum; it's an investment in
+              yourself, your company, and your community. Join us today and
+              experience the difference that local, accessible leadership
+              support can make in your professional journey. Together, we'll
+              share our wins and losses, brainstorm, and ideate to become more
+              effective leaders, ultimately benefiting our companies and
+              communities.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
