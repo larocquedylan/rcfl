b98f1ee647767b1ac4ec140795c79cdae2c5f4e5
Dylan L <dylancblarocque@gmail.com>
2023-05-16 10:16:45 -0700

    add call to action and edit copy

--- src/app/rclf/page.tsx
+++ src/app/rclf/page.tsx
@@ -1,3 +1,4 @@
+import CallToAction from '@/components/CallToAction';
 import Navbar from '@/components/NavBar';
 import NextPage from '@/components/NextPage';
 import React from 'react';
@@ -9,16 +10,25 @@ const WhyRclf: React.FC<WhyRclfProps> = ({}) => {
     <div className='flex flex-col justify-center m-2 md:flex-row md:m-12'>
       <Navbar />
       <main className='flex flex-col items-center justify-between px-0 lg:px-16'>
-        <div className='grid w-full max-w-5xl my-8 md:text-left lg:mb-0 lg:grid-cols-1 lg:text-left'>
+        <div className='grid w-full max-w-5xl my-4 md:my-8 md:text-left lg:mb-0 lg:grid-cols-1 lg:text-left'>
           <div className='px-4 py-0 md:px-5 md:py-4'>
             <h2 className='mb-3 text-2xl font-semibold'>
               Discover the RCLF Difference
             </h2>
             <p className='text-sm lg:text-base'>
-              RCLF is the leadership forum that prioritizes accessibility,
-              affordability, and giving back. We offer streamlined leadership
-              support at just $200 per month, and we donate 20% of our revenues
-              to support our community.
+              RCLF is the Edmonton leadership forum that prioritizes
+              accessibility, affordability, and giving back. We offer
+              streamlined leadership support at just $200 per month, and we
+              donate 20% of our revenues to support our community.
+            </p>
+            <p className='mt-4 text-sm lg:text-base'>
+              RCLF ensures <i>Confidentiality</i> and <i>Committment</i> from
+              all members. If a member misses more than 1.5 meetings, their
+              place in the group will be voted upon by the group. Breach of
+              Confidentiality will result in immediate removal from the group.
+              The chair of the group will rotate amongst members, the chair will
+              be in charge of providing the facility and facilitating the
+              session.
             </p>
             <h3 className='mt-4 text-xl font-semibold'>
               Our Unique Operating Principles:
@@ -32,8 +42,8 @@ const WhyRclf: React.FC<WhyRclfProps> = ({}) => {
               </li>
               <li>
                 <b>Relatable: </b> We structure our groups around age to ensure
-                relatable and meaningful conversations. All members are within
-                five years of the average group age.
+                relatable conversations. All members are within five years of
+                the average group age.
               </li>
               <li>
                 <b>Intimate: </b> We limit our groups to 6-8 members to allow
@@ -58,6 +68,7 @@ const WhyRclf: React.FC<WhyRclfProps> = ({}) => {
                 community and building a better Edmonton
               </li>
             </ul>
+            <CallToAction />
           </div>
         </div>
         <hr className='mt-6 border-t border-gray-200' />
