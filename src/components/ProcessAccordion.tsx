64fe4c703898919c68aa589bc7663c48bbb6ddc1
Dylan L <dylancblarocque@gmail.com>
2023-05-17 14:15:47 -0700

    edit application

--- src/components/ProcessAccordion.tsx
+++ src/components/ProcessAccordion.tsx
@@ -7,25 +7,14 @@ import { ChevronDownIcon } from '@radix-ui/react-icons';
 const ProcessAccordion = () => (
   <Accordion.Root
     type='multiple'
-    defaultValue={[
-      'step1',
-      'step2',
-      'step3',
-      'step4',
-      'step5',
-      'step6',
-      'step7',
-    ]}
+    defaultValue={['step1', 'step2', 'step3', 'step4', 'step5']}
   >
     <AccordionItem value='step1' title='Step 1: Initial Interest'>
-      You&apos;ve heard about our leadership forum and you&apos;re intrigued.
-      You&apos;ve visited our website, and you now understand about 70% of what
-      we&apos;re all about. You&apos;re ready for the next step.
+      Explore our website and gain an understanding of what we offer.
     </AccordionItem>
 
     <AccordionItem value='step2' title='Step 2: Get to Know Us'>
-      Let&apos;s connect on a deeper level. Schedule an unstructured interview
-      with us through our{' '}
+      Schedule an unstructured interview with our{' '}
       <a
         href='https://calendly.com/rclf'
         target='_blank'
@@ -39,56 +28,31 @@ const ProcessAccordion = () => (
     </AccordionItem>
 
     <AccordionItem value='step3' title='Step 3: Apply'>
-      Feeling inspired and ready to take the next step? Fill out our application
-      form{' '}
+      Fill out our application form{' '}
       <a
         href='https://docs.google.com/forms/d/e/1FAIpQLScp9yVWKb8SShYUWskTzCu2Gv68mZ_HfqMC8G6gNT3SKpeV-Q/viewform?vc=0&c=0&w=1&flr=0'
         target='_blank'
         rel='noopener noreferrer'
         className='font-bold text-blue-500'
       >
-        Here
-      </a>
-      . This is your formal commitment to our community. To solidify your
-      commitment, we ask for a $200 deposit upon completion of your application.
+        here
+      </a>{' '}
+      and provide a $200 deposit to formalize your commitment.
     </AccordionItem>
 
     <AccordionItem value='step4' title='Step 4: Curation'>
-      Once your application is in, our team will get to work. We&apos;ll review
-      your profile, consider your business needs, and start the matching
-      process.{' '}
+      Our team will review your profile, match you with a suitable group, and
+      aim to place you within 90 days.{' '}
       <span className='font-semibold'>
-        We aim to place you in a suitable group within 90 days. If we can&apos;t
-        find a fit within six months, we&apos;ll refund your deposit.
+        If no fit is found within six months, your deposit will be refunded.
       </span>
     </AccordionItem>
 
     <AccordionItem value='step5' title='Step 5: Acceptance'>
-      If we find a pre-existing group that fits your profile, we&apos;ll
-      introduce your profile to current members. They&apos;ll have a chance to
-      vote on your admission. If the majority votes you in, you&apos;re part of
-      the team.
-      <span className='font-semibold'>
-        If a suitable existing group isn&apos;t available, we&apos;ll work on
-        forming a new one and the 6-month period will continue. If we can&apos;t
-        find a fit within six months, we&apos;ll refund your deposit.
-      </span>
+      Your profile will be introduced to current members for a vote. If
+      accepted, you&apos;ll join the group and start collaborating with fellow
+      members.
     </AccordionItem>
-
-    <AccordionItem value='step6' title='Step 6: Placement'>
-      Once your group is ready, it&apos;s time to join in. Yo&apos;ll meet your
-      fellow group members, start collaborating, and begin reaping the benefits
-      of our dynamic community.
-    </AccordionItem>
-
-    <AccordionItem value='step7' title='Step 7: Collaborate'>
-      Now that you&apos;re part of a group, it&apos;s time to roll up your
-      sleeves and get to work. Remember, this is a collaborative journey.
-      We&apos;ll check in with you after your first and third meeting to ensure
-      that you&apos;re getting the value you expect from the program.
-    </AccordionItem>
-
-    {/* Repeat for other steps */}
   </Accordion.Root>
 );
 
