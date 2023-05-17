f086a0e6536e5ef32f28e72dbbf509bee061e1b5
Dylan L <dylancblarocque@gmail.com>
2023-05-16 17:07:45 -0700

    add footer

--- src/app/process/page.tsx
+++ src/app/process/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import Navbar from '@/components/NavBar';
 import NextPage from '@/components/NextPage';
+import Footer from '@/components/Footer';
 
 const ProcessAccordion = dynamic(
   () => import('../../components/ProcessAccordion'),
@@ -13,7 +14,7 @@ const ProcessAccordion = dynamic(
 const ProcessPage = () => (
   <div className='flex flex-col justify-center m-2 md:flex-row md:m-12'>
     <Navbar />
-    <main className='flex flex-col items-center justify-between w-full min-h-screen px-0 lg:px-16'>
+    <main className='flex flex-col items-center justify-between px-0 min-h-max lg:px-16'>
       <div className='grid w-full max-w-5xl my-4 md:my-8 md:text-left lg:mb-0 lg:grid-cols-1 lg:text-left'>
         <div className='px-4 py-0 md:px-5 md:py-4'>
           <h2 className='mb-3 text-2xl font-semibold'>Your RCLF journey</h2>
@@ -24,6 +25,7 @@ const ProcessPage = () => (
       <div className='flex justify-between w-full'>
         <NextPage prevPage='/rclf' nextPage='/' />
       </div>
+      <Footer />
     </main>
   </div>
 );
