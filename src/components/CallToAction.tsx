efb4ae9bbed03d694d080e639aafbad9b9e5333b
Dylan L <dylancblarocque@gmail.com>
2023-05-16 17:10:06 -0700

    edit CTA copy

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -5,18 +5,26 @@ interface CallToActionProps {}
 const CallToAction: React.FC<CallToActionProps> = ({}) => {
   return (
     <p className='mt-4 text-sm font-semibold lg:text-base'>
-      If you are interested in learning more about RCLF,{' '}
+      If you are interested in learning more about RCLF and how our leadership
+      forum can benefit you,
       <a
         href='https://calendly.com/rclf'
         target='_blank'
         rel='noopener noreferrer'
-        className='font-bold text-blue-500'
+        className='font-bold text-blue-500 hover:text-edmonton'
       >
         {' '}
-        Book a call with us!
-      </a>{' '}
-      We would be happy to provide some value, answer questions, and help
-      determine if RCLF is a good fit for you.
+        book a coffee with us!{' '}
+      </a>
+      Or, jump straight to our application form and apply{' '}
+      <a
+        href='https://docs.google.com/forms/d/e/1FAIpQLScp9yVWKb8SShYUWskTzCu2Gv68mZ_HfqMC8G6gNT3SKpeV-Q/viewform?vc=0&c=0&w=1&flr=0'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='font-bold text-blue-500 hover:text-edmonton'
+      >
+        here.
+      </a>
     </p>
   );
 };
